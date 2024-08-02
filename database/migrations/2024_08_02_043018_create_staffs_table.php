a2f244727eafe1e47be5bbcb534f4bf2ec570611
Duc Tri <pcldt123@gmail.com>
2024-08-02 15:53:40 +0700

    commit lần ba

---
+++ database/migrations/2024_08_02_043018_create_staffs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStaffsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('staffs', function (Blueprint $table) {
+            $table->id();
+            $table->string('full_name', 50);
+            $table->foreignId('department_id')->constrained()->onDelete('cascade');
+            $table->string('photo')->nullable();
+            $table->text('bio')->nullable();
+            $table->string('salary_type', 50);
+            $table->decimal('salary_amount', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('staffs');
+    }
+};
